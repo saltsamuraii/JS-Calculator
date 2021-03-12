51625d55d01a1514056ac6b80914d613181f18ff
Kirill <piksarus@gmail.com>
2021-03-12 16:50:30 +0300

    add alert on operation + input(not working now)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let btnDivide = document.querySelector('#buttonDivide')
 let number1 = document.querySelector('#number1')
 let number2 = document.querySelector('#number2')
 
-//let result = document.querySelector('#result').value
+let result = document.querySelector('#result').value
 
 
 function sum() {
